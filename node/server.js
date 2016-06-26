28ceb23a4a6b8d343b5109ef9339e12c915598ab
Tigran Tsaturyan <tigran@Tigrans-MacBook-Air.local>
2016-06-26 08:45:39 -0700

    title option

--- node/server.js
+++ node/server.js
@@ -59,15 +59,16 @@ app.route('/upload_file')
         req.pipe(req.busboy);
         req.busboy.on('file', function (fieldname, file, filename) {
             var file_id = req.query.file_id;
+            var title = req.query.title;
             console.log("Uploading: " + filename + " id = " + file_id);
             fstream = fs.createWriteStream(__dirname + '/files/' + file_id);
             file.pipe(fstream);
             console.log("Created pipe")
             fstream.on('close', function () {    
-                console.log("Upload Finished of " + filename);    
+                console.log("Upload Finished for " + title);    
                 task_status = db_gate.create_task_id(file_id);
                 // TODO: use date or timestamp instead of filename
-                business_logic.start_audio_processing(file_id, filename);
+                business_logic.start_audio_processing(file_id, title);
                 res.json(task_status);
             });
         });
