be5027483a0fd2238cba06b6d56f715f2d623f3e
Tigran Tsaturyan <tigran@Tigrans-MacBook-Air.local>
2016-06-25 21:23:28 -0700

    analyzer

--- node/analyzers.js
+++ node/analyzers.js
@@ -1,7 +1,25 @@
 module.exports = {
-  analyze_words: function (word_data) {
-    list_of_words = word_data.actions[0].result.document;
-    // whatever
+    /*
+        return a list of numbers representing words said per second.
+    */
+  analyze_words: function (words_time) {
+    var list_of_words = words_time.actions[0].result.document;
+    if (list_of_words.length == 0) {
+        console.warn("list_of_words.length == 0")
+        return [];
+    }
+    var last_duration = list_of_words[list_of_words.length - 1].offset;
+    var total_sec = Math.ceil(last_duration / parseFloat(1000));
+    var seconds_stats = new Array(total_sec);
+    for (var i = 0; i < seconds_stats.length; i++) {
+        seconds_stats[i] = 0;
+    }
+    console.log("at point 12331");
+    for (var i = 0; i < list_of_words.length; i++) {
+        sec = Math.floor(list_of_words[i].offset / 1000);
+        seconds_stats[sec]++;
+    }
+    return seconds_stats;
   },
   bar: function () {
     // whatever
