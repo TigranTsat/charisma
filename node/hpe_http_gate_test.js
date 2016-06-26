efcaf8bf45a0097fb8115d9f04e009cf093b8ef1
Tigran Tsaturyan <tigran@Tigrans-MacBook-Air.local>
2016-06-25 22:08:13 -0700

    Added lowdb

--- node/hpe_http_gate_test.js
+++ node/hpe_http_gate_test.js
@@ -1,4 +1,6 @@
 var hpe_http_gate = require('./hpe_http_gate.js')
+var db_gate = require("./db_gate");
+db_gate.test_db();
 // TODO: place with real wav file
 hpe_http_gate.recognizespeech('simple_audio1.m4a', function() {
 })
